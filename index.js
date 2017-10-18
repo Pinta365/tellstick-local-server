a12ad6df7e62d4ffc667e9095f96a8462b3e8a1b
Pintapoff! <pintapoff@GitHub>
2017-10-18 23:55:50 +0200

    .

--- index.js
+++ index.js
@@ -52,34 +52,128 @@ class TellstickLocalServer {
         req.end();
     }
 
-}
+    sensorsList(params, callback) {
+        // params is optional for this call so we better check if it's supplied or not 
+        if ((!callback) && (typeof params === 'function')) {
+            callback = params;
+            params = null;
+        }
 
-TellstickLocalServer.prototype.sensorsList = function(params, callback) {
-
-    // params is optional for this call so we better check if it's supplied or not 
-    if ((!callback) && (typeof params === 'function')) {
-        callback = params;
-        params = null;
+        this.getData('sensors/list', params, function(err, data){ 
+            callback(err, data);
+        });
     }
 
-    this.getData('sensors/list', params, function(err, data){ 
-        callback(err, data);
-    });
+    sensorInfo(params, callback){
+
+        this.getData('sensor/info', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    sensorSetName(params, callback){
+
+        this.getData('sensor/setName', params, function(err, data){ 
+            callback(err, data);
+        });
+    } 
+
+    devicesList(params, callback) {
+        // params is optional for this call so we better check if it's supplied or not 
+        if ((!callback) && (typeof params === 'function')) {
+            callback = params;
+            params = null;
+        }
+
+        this.getData('devices/list', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceBell(params, callback){
+
+        this.getData('device/bell', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceCommand(params, callback){
+
+        this.getData('device/command', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceDim(params, callback){
+
+        this.getData('device/dim', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceDown(params, callback){
+
+        this.getData('device/down', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceInfo(params, callback){
+
+        this.getData('device/info', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceLearn(params, callback){
+
+        this.getData('device/learn', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceSetName(params, callback){
+
+        this.getData('device/setName', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceStop(params, callback){
+
+        this.getData('device/stop', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceTurnOff(params, callback){
+
+        this.getData('device/turnOff', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceTurnOn(params, callback){
+
+        this.getData('device/turnOn', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    deviceUp(params, callback){
+
+        this.getData('device/up', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
+
+    luaCall(params, callback){
+
+        this.getData('lua/call', params, function(err, data){ 
+            callback(err, data);
+        });
+    }
 }
-
-TellstickLocalServer.prototype.sensorInfo = function(params, callback){
-
-    this.getData('sensor/info', params, function(err, data){ 
-        callback(err, data);
-    });
-}
-
-TellstickLocalServer.prototype.sensorSetName = function(params, callback){
-
-    this.getData('sensor/setName', params, function(err, data){ 
-        callback(err, data);
-    });
-} 
 
 module.exports = TellstickLocalServer;
 
